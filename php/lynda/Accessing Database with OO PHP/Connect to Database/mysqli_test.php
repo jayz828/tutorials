8f4a30314278d73edec2bcfb0ab6dcc16a8a6167
Jason Natividad <jainatividad@yahoo.com>
2016-06-27 22:40:53 -0700

    Completed Connecting to using MySQLi

---
+++ php/lynda/Accessing Database with OO PHP/Connect to Database/mysqli_test.php
@@ -0,0 +1,39 @@
+<?php 
+
+	try{ 
+
+		require_once 'includes/mysqli_connect.php';
+	
+
+	} catch (Exception $e) {
+		$error = $e->getMessage();
+	}
+
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>MySQLi Connection</title>
+</head>
+<body>
+
+	<h1>Connecting with MySQLi</h1>
+
+	<?php
+
+		if (isset($error)) {
+			echo "<p>$error</p>";
+		} else {
+			echo "<p>Connection successful.</p>";
+		}
+
+		$db->close();
+	 ?>
+	
+</body>
+</html>
