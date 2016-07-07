cbe506f4d81f6e284a1147bc50d355df42342196
Jason Natividad <jainatividad@yahoo.com>
2016-07-07 00:20:22 -0700

    Compete Creating an instance of a class from a result set

---
+++ php/lynda/Accessing Database with OO PHP/Creating an instance of a class from a result set/mysqli_class.php
@@ -0,0 +1,26 @@
+<?php 
+
+	try {
+		require_once 'includes/mysqli_connect.php';
+		require_once 'Car.php';
+
+		// Set Car ID
+		$car_id = 25;
+
+		// Get the car's details
+
+		$sql = "SELECT * FROM cars 
+				LEFT JOIN makes USING (make_id)
+				WHERE car_id = $car_id";
+		$result = $db->query($sql); 
+		$car = $result->fetch_object('Car', array($car_id));
+		echo $car;
+
+	} catch (Exception $e) {
+		$error = $e->getMessage();
+	}
+
+	if (isset($error)) {
+		echo $error;
+	}
+ ?>
