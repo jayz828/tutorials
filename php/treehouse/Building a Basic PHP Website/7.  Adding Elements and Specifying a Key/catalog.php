89ece7201500e519eb70295df89445d4f2f45474
Jason Natividad <jainatividad@yahoo.com>
2016-07-11 22:45:02 -0700

    Complete Adding Elements and Specifying a key

---
+++ php/treehouse/Building a Basic PHP Website/7.  Adding Elements and Specifying a Key/catalog.php
@@ -0,0 +1,56 @@
+
+
+<?php 
+
+$catalog = array();
+$catalog[101] = "Design Pattern";
+$catalog[201] = "Forrest Gump"; 
+$catalog[301] ="Beethoven";
+
+$catalog[102] = "Clean Code";
+
+$pageTitle = "Full Catalog";
+$section = null;
+
+
+if (isset($_GET["cat"])) {
+
+
+
+if ($_GET["cat"] == "books") {
+	$pageTitle = "Books";
+	$section = "books";
+} else if ($_GET["cat"] == "movies"){
+	$pageTitle ="Movies";
+	$section = "movies";
+} else if ($_GET["cat"] == "music") {
+	$pageTitle = "Music";
+	$section = "music";
+}
+
+}
+
+
+include("includes/header.php") ?>
+
+
+<div class="section catalog page">
+
+	<div class="wrapper">
+		<h1><?php echo $pageTitle; ?></h1>
+
+		<ul>
+
+			<?php foreach($catalog as $item) {
+				echo "<li>" . $item . "</li>";
+				}
+				?>
+
+		</ul>
+	</div>
+
+
+	
+</div>
+
+<?php include("includes/footer.php"); ?>
