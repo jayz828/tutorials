5eac7b15e74c49f62c3e234a50c3da86d48dc2bc
Jason Natividad <jainatividad@yahoo.com>
2016-07-12 21:10:02 -0700

    Complete Display Categories tutorial

---
+++ php/treehouse/Building a Basic PHP Website/12.  Displaying Categories/includes/functions.php
@@ -0,0 +1,30 @@
+<?php 
+
+function get_item_html($id, $item) {
+
+				$output = "<li><a href='#'><img src='" 
+				. $item["img"] . "' alt='"
+				. $item["title"] . "' />"
+				. "<p>View Details</p>"
+				. "</a></li>";
+				
+				
+				return $output;
+}
+
+function array_category($catalog, $category) {
+	$output = array();
+
+	if ($category == null) {
+		return array_keys($catalog);
+	}
+
+	foreach ($catalog as $id => $item) {
+		if (strtolower($category) == strtolower($item["category"])) {
+			$output[] = $id;
+
+		}
+	}
+
+	return $output;
+}
