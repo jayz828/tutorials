9a2f89aa8fe1d847357551165353ecb801d6300f
Jason Natividad <jainatividad@yahoo.com>
2016-07-14 22:50:31 -0700

    Complete Creating input fields tutorial

---
+++ php/treehouse/Building a Basic PHP Website/18.  Creating input fields/suggest.php
@@ -0,0 +1,43 @@
+
+
+<?php 
+
+$pageTitle = "Suggest a Media Item";
+$section = "suggest";
+
+include("includes/header.php") ?>
+
+
+
+<div class="section page">
+
+		<div class="wrapper">
+	
+			<h1>Suggest a Media Item</h1>
+			<p>If you think there is something I&rsquo;m missing, let me know!  Complete the form to send me an email</p>
+
+			<form method="post" action="">
+				
+				<table>
+					<tr>
+						<th><label for="name">Name</label></th>
+						<td><input type="text" name="name" id="name"></td>
+					</tr>
+					<tr>
+						<th><label for="email">Email</label></th>
+						<td><input type="text" name="email" id="email"></td>
+					</tr>
+					<tr>
+						<th><label for="details">Suggest Item Details</label></th>
+						<td><textarea name="details" id="details"></textarea></td>
+					</tr>
+				</table>
+
+				<input type="submit" value="Send">
+				
+			</form>
+
+		</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
