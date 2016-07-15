89fee13abaca8a39dd1ac63d59bc43e5ffdc6ea9
Jason Natividad <jainatividad@yahoo.com>
2016-07-14 22:36:25 -0700

    Complete Setting up the initial form tutorial

---
+++ php/treehouse/Building a Basic PHP Website/17.  Setting up the intial form/suggest.php
@@ -0,0 +1,29 @@
+
+
+<?php 
+
+$pageTitle = "Suggest a Media Item";
+$section = "suggest";
+
+include("includes/header.php") ?>
+
+
+
+<div class="section page">
+
+		<div class="wrapper">
+	
+			<h1>Suggest a Media Item</h1>
+			<p>If you think there is something I&rsquo;m missing, let me know!  Complete the form to send me an email</p>
+
+			<form method="post" action="">
+				
+
+				<label for="name">Name</label><input type="text" name="name" id="name">
+
+			</form>
+
+		</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
