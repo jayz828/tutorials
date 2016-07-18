241b3424e53b3706e155ed1bdae4c367d3abb237
Jason Natividad <jainatividad@yahoo.com>
2016-07-17 22:34:59 -0700

    Complete Checking the Request method tutorial

---
+++ php/treehouse/Building a Basic PHP Website/22.  Checking the Request Method/suggest.php
@@ -0,0 +1,85 @@
+<?php 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+
+		// var_dump($_POST);
+
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$details = $_POST["details"];
+
+
+		echo "<pre>";
+		$email_body = "";
+		$email_body .= "Name " . $name . "\n";
+		$email_body .= "Email " . $email . "\n";
+		$email_body .= "Details ". $details . "\n" ;
+
+		echo $email_body;
+		echo "</pre>";
+
+		// To Do: Send E-mail
+
+		$pageTitle = "Thank you";
+		$section = null;
+
+
+		header("location:suggest.php?status=thanks");
+
+}
+?>
+
+<?php 
+
+$pageTitle = "Suggest a Media Item";
+$section = "suggest";
+
+include("includes/header.php") ?>
+
+
+
+<div class="section page">
+
+		<div class="wrapper">
+	
+			<h1>Suggest a Media Item</h1>
+
+			<?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+
+				echo "<h1>Thank you</h1>" . "<br>" . 
+		"<p>Thanks for the email.  I&rsquo;ll check out your suggestion shortly!</p>";
+
+				} else {?>
+
+			<p>If you think there is something I&rsquo;m missing, let me know!  Complete the form to send me an email</p>
+
+			<form method="post" action="suggest.php">
+				
+				<table>
+					<tr>
+						<th><label for="name">Name</label></th>
+						<td><input type="text" name="name" id="name"></td>
+					</tr>
+					<tr>
+						<th><label for="email">Email</label></th>
+						<td><input type="text" name="email" id="email"></td>
+					</tr>
+					<tr>
+						<th><label for="details">Suggest Item Details</label></th>
+						<td><textarea name="details" id="details"></textarea></td>
+					</tr>
+				</table>
+
+				<input type="submit" value="Send">
+				
+			</form>
+
+			<?php }?>
+
+
+		</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
