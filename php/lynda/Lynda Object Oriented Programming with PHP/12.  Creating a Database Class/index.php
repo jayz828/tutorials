9487590198c6cd4539e559ed655f8c5ffbd59e47
Jason Natividad <jainatividad@yahoo.com>
2016-07-20 21:27:21 -0700

    Complete Creating a Databasee class tutorial

---
+++ php/lynda/Lynda Object Oriented Programming with PHP/12.  Creating a Database Class/index.php
@@ -0,0 +1,39 @@
+<?php  
+
+require 'includes/classAddress.php';
+require 'includes/classDatabase.php';
+
+echo '<h2>Instantiating Address</h2>';
+
+$address = new Address;
+
+
+
+echo '<h2>Setting properties....</h2>';
+$address->street_address_1 = "555 Fake Street";
+$address->city_name = "Townsville";
+$address->subdivision_name = "State";
+$address->country_name = "United States of America";
+$address->address_type_id = 1;
+
+echo $address;
+
+
+
+echo '<h2>Testing address __construct with an array</h2>';
+
+$address_2 = new Address(array (
+	'street_address_1' => '123 Phony Ave',
+	'city_name' => 'Villageland',
+	'subdivision_name' => 'Region',
+
+	'country_name' => 'Canada',
+	));
+
+echo $address_2->display();
+echo '<h2>Address __toString</h2>';
+echo $address_2;
+
+
+
+?>
