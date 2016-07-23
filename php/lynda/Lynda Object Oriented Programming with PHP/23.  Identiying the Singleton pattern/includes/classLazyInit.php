a3e23c076c3b0144f28ae782f995bd7f46db71c5
Jason Natividad <jainatividad@yahoo.com>
2016-07-22 22:59:32 -0700

    Complete Identifying the singleton pattern and Using the factory pattern method

---
+++ php/lynda/Lynda Object Oriented Programming with PHP/23.  Identiying the Singleton pattern/includes/classLazyInit.php
@@ -0,0 +1,16 @@
+<?php 
+
+class LazyInit {
+	protected _property;
+
+	function getProperty() {
+
+		if (is_null($this->_property)) {
+			$this->_property = 'init';
+		}
+
+		return $this->_property;
+
+	}
+	
+}
