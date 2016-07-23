621b7162c9194445d5f00abee1904a2f7d247f4e
Jason Natividad <jainatividad@yahoo.com>
2016-07-22 20:20:18 -0700

    Complete Leveraging standard objecs from the database

---
+++ php/lynda/Lynda Object Oriented Programming with PHP/19.  Leveraging standard class objects/includes/classAddressResidence.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+	/*  
+	*	Residential Address
+	*/
+
+	class AddressResidence extends Address {
+
+		/*
+		*	Initialization
+		*/
+
+		protected function _init() {
+			$this->_setAddressTypeId(Address::ADDRESS_TYPE_RESIDENCE);
+		}
+	}
